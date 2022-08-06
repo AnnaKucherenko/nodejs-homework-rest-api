23eed1a9d52f1a9b8220ecc8a9ac856057f9c31a
AnnaKucherenko <annakucherenko31@gmail.com>
2022-08-06 14:31:14 +0300

    додає аватар та оновлює його

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -7,7 +7,7 @@ const getAll = async (req, res) => {
       const {page = 1, limit = 20, favorite} = req.query;
       const skip = (page-1)*limit;
       console.log(skip)
-      const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)})
+      const result = await Contact.find({owner, favorite}, "-createdAt -updatedAt", {skip, limit: Number(limit)})
             .populate("owner", "name email");
       res.json(result);
 }
---
+++ middlewares/upload.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+const imageProcessing = require("../helpers/imageProcessing");
+
+const tempDir = path.join(process.cwd(), "temp");
+
+const multerConfig = multer.diskStorage({
+    destination: tempDir,
+    filename: (req, file, cb)=>{
+           
+        const fileName = file.originalname;
+        // console.log(tempDir)
+        // imageProcessing(tempDir)
+        console.log(fileName)
+        cb(null, fileName);
+    }
+});
+
+const upload = multer({
+    storage:multerConfig
+});
+
+module.exports = upload;
